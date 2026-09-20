f538f71d341e9343ddc58207927b253f5626ada5
Arnav Verma <109273538+Arnav7107@users.noreply.github.com>
2026-09-20 22:26:51 +0530

    Time: 0 ms (100%), Space: 19.3 MB (41.77%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -8,7 +8,19 @@ class Solution:
         if(nums[n-1] > nums[n-2]):
             return n-1
 
-        for i in range(1, len(nums) - 1):
-            if(nums[i] > nums[i-1] and nums[i] > nums[i+1]):
-                return i
+        # for i in range(1, len(nums) - 1):
+        #     if(nums[i] > nums[i-1] and nums[i] > nums[i+1]):
+        #         return i
+
+        start = 1
+        end = n-2
+        while(start <= end):
+            mid = start + (end - start)//2
+            if(nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]):
+                return mid
+            elif(nums[mid] < nums[mid-1]):
+                end = mid - 1
+            elif(nums[mid] < nums[mid+1]):
+                start = mid + 1
         
+        return -1      
